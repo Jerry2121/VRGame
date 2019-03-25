ca57772c2cd160f2b17386235564a91dc4c7b35b
Jerry2121 <jerry2121121021@yahoo.com>
2019-03-25 10:07:07 -0700

    made UI parent with the VR player camera

---
+++ VRProject/Assets/UIVRparent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIVRparent : MonoBehaviour
+{
+    public Transform VRPlayer;
+    
+    void Update()
+    {
+        this.gameObject.transform.position = VRPlayer.position;
+        this.gameObject.transform.rotation = VRPlayer.rotation;
+    }
+}
