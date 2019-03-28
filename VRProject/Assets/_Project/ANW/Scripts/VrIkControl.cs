54c7572d96eed55f08749ace587365834d16d105
Nathan Weis <DrakonRider598@yahoo.com>
2019-03-28 10:30:38 -0700

    IK Feet

--- VRProject/Assets/_Project/ANW/Scripts/VrIkControl.cs
+++ VRProject/Assets/_Project/ANW/Scripts/VrIkControl.cs
@@ -10,10 +10,18 @@ public class VrIkControl : MonoBehaviour
 
     public Transform rightHandObj;
     public Transform leftHandObj;
+
+    public Transform rightFoot;
+    public Transform leftFoot;
+    public Transform rightKnee;
+    public Transform leftKnee;
+
     public Transform ovrCamera;
     public Transform trackingSpace;
     public Transform headObj;
-    public Vector3 playerOffset;
+    public int playerYOffset;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +31,12 @@ public class VrIkControl : MonoBehaviour
 
     private void Update()
     {
-        this.gameObject.transform.position = ovrCamera.transform.position - playerOffset;
+        this.gameObject.transform.position = ovrCamera.transform.position - new Vector3 (ovrCamera.transform.position.x, playerYOffset, ovrCamera.transform.position.z);
         headObj.transform.rotation = trackingSpace.transform.rotation;
+        if (trackingSpace.position.y < -0.5)
+        {
+            // Set animator croush to true
+        }
     }
 
     // A callback for calculating IK
@@ -32,14 +44,33 @@ public class VrIkControl : MonoBehaviour
     {
         animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
         animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
 
         animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
         animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1);
 
         animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandObj.position);
         animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
 
         animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandObj.rotation);
         animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandObj.rotation);
+
+        // If the players foot is lower than the ground, position back up.
+        // This will stop the tracking spaces movement from clipping the player through the ground.
+        if (rightFoot.position.y < 0.5)
+        {
+            animator.SetIKPosition(AvatarIKGoal.RightFoot, new Vector3 (rightFoot.position.x, 0.1100572f, rightFoot.position.z));
+            animator.SetIKPosition(AvatarIKGoal.LeftFoot, new Vector3 (leftFoot.position.x, 0.1100572f, leftFoot.position.z));
+           
+            Vector3 foo = rightFoot.transform.rotation.eulerAngles;
+            foo = -rightKnee.transform.rotation.eulerAngles;
+            foo.z += 90;
+            foo.y -= 90;
+            rightFoot.transform.rotation = Quaternion.Euler(foo);
+            animator.SetIKRotation(AvatarIKGoal.RightFoot, Quaternion.Euler(foo));
+        }
     }
 }
