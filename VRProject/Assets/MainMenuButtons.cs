0519d8853963af139d3b6790967b5528f5fd7b85
Jerry2121 <jerry2121121021@yahoo.com>
2019-03-29 10:30:23 -0700

    MainMenuChanges

---
+++ VRProject/Assets/MainMenuButtons.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public GameObject MainCanvas;
+    public GameObject PlayCanvas;
+    public GameObject PlayMain;
+    public GameObject PlayHost;
+    public GameObject PlayJoin;
+    public GameObject SettingsCanvas;
+    public GameObject ExitConfirm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        MainCanvas.SetActive(!MainCanvas.activeSelf);
+        PlayCanvas.SetActive(!PlayCanvas.activeSelf);
+        PlayMain.SetActive(true);
+        PlayHost.SetActive(false);
+        PlayJoin.SetActive(false);
+    }
+    public void HostGame()
+    {
+        PlayMain.SetActive(false);
+        PlayHost.SetActive(true);
+        PlayJoin.SetActive(false);
+    }
+    public void CreateGameTEST()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void JoinGame()
+    {
+        PlayMain.SetActive(false);
+        PlayHost.SetActive(false);
+        PlayJoin.SetActive(true);
+    }
+    public void GoBackMain()
+    {
+        MainCanvas.SetActive(!MainCanvas.activeSelf);
+        PlayCanvas.SetActive(!PlayCanvas.activeSelf);
+        PlayMain.SetActive(false);
+        PlayHost.SetActive(false);
+        PlayJoin.SetActive(false);
+    }
+    public void GoBackHost()
+    {
+        PlayMain.SetActive(true);
+        PlayHost.SetActive(false);
+        PlayJoin.SetActive(false);
+    }
+    public void GoBackJoin()
+    {
+        PlayMain.SetActive(true);
+        PlayHost.SetActive(false);
+        PlayJoin.SetActive(false);
+    }
+    public void Settings()
+    {
+
+    }
+    public void ExitGame()
+    {
+        ExitConfirm.SetActive(!ExitConfirm.activeSelf);
+    }
+    public void ExitYes()
+    {
+        Application.Quit();
+    }
+}
