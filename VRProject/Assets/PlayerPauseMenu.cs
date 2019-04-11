6d524c69fbe1687fb4c8975c42cc48bc86099b63
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-11 10:21:59 -0700

    Changes in my scene and IK Testing scene

---
+++ VRProject/Assets/PlayerPauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPauseMenu : MonoBehaviour
+{
+    public GameObject PauseCanvas;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            Debug.Log("foo");
+            PauseCanvas.transform.position = Player.transform.position + new Vector3(0,0,1);
+            PauseCanvas.transform.rotation = Player.transform.rotation;
+            PauseCanvas.SetActive(!PauseCanvas.activeSelf);
+        }
+    }
+}
