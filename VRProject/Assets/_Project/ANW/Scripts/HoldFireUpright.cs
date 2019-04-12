3a98e05c7c6bd6d1c7a1ee83228bc773cde3f36f
Nathan Weis <DrakonRider598@yahoo.com>
2019-04-12 09:27:41 -0700

    Fire particle improvements

---
+++ VRProject/Assets/_Project/ANW/Scripts/HoldFireUpright.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldFireUpright : MonoBehaviour
+{
+    public Vector3 uprightRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(uprightRotation.x, uprightRotation.y, uprightRotation.z);
+    }
+}
