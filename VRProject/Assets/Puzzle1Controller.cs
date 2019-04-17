0dde2b758c078e84f61dc073e3c089b20a57c00f
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-17 09:32:41 -0700

    Changes to my scene, Working on Puzzle1

---
+++ VRProject/Assets/Puzzle1Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1Controller : MonoBehaviour
+{
+    public bool PuzzleStarted;
+    public bool PuzzleCompleted;
+    public bool PuzzlePoweredUp;
+    public Light Puzzle1Light;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "P1Start" && !PuzzleStarted && !PuzzleCompleted && PuzzlePoweredUp)
+        {
+            Puzzle1Light.color = new Color32(0, 255, 0, 255);
+            PuzzleStarted = true;
+        }
+        if(other.gameObject.tag == "P1Bar" && PuzzleStarted && PuzzlePoweredUp)
+        {
+            Puzzle1Light.color = new Color32(255, 0, 0, 255);
+            PuzzleStarted = false;
+
+        }
+        if (other.gameObject.tag == "P1End" && PuzzleStarted && PuzzlePoweredUp)
+        {
+            Puzzle1Light.color = new Color32(0, 0, 255, 255);
+            PuzzleStarted = false;
+            PuzzleCompleted = true;
+        }
+    }
+}
---
+++ VRProject/Assets/Puzzle1WheelController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1WheelController : MonoBehaviour
+{
+    public Vector3 Wheelposition;
+    public float WheelRotateX;
+    public float WheelRotateY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wheelposition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "VRHands")
+        {
+            this.transform.position = Wheelposition;
+            Quaternion rot = transform.rotation;
+            Vector3 foo = rot.eulerAngles;
+            foo.x = WheelRotateX;
+            foo.y = WheelRotateY;
+            this.transform.rotation = Quaternion.Euler(foo);
+        }
+    }
+}
