1a3220f68f633b4eb25d7bb4bcfb5a0efb315fe0
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-19 09:43:54 -0700

    Test changes

--- VRProject/Assets/Puzzle1WheelController.cs
+++ VRProject/Assets/Puzzle1WheelController.cs
@@ -13,10 +13,12 @@ public class Puzzle1WheelController : MonoBehaviour
     public bool grabbed;
 
     public float angle;
+    Vector3 forward;
 
     void Start()
     {
         Wheelposition = this.transform.position;
+        forward = transform.forward;
     }
 
     // Update is called once per frame
@@ -24,43 +26,49 @@ public class Puzzle1WheelController : MonoBehaviour
     {
         if (grabbed)
         {
-            Debug.Log("Hand position prior = " + grabbedBy.transform.position);
+            //Debug.Log("Hand position prior = " + grabbedBy.transform.position);
 
             Vector3 handPosition = grabbedBy.transform.position;
 
-            Debug.Log("Hand position after = " + handPosition);
+            //Debug.Log("Hand position after = " + handPosition);
 
             Vector3 handPositionResetXZ = new Vector3(handPosition.x, handPosition.y, 0);
 
-            Debug.Log("Hand position reset = " + handPositionResetXZ);
+            //Debug.Log("Hand position reset = " + handPositionResetXZ);
 
             Vector3 handDirection = transform.position - handPositionResetXZ;
 
-            Debug.Log("Hand direction prior = " + handDirection);
+            //Debug.Log("Hand direction prior = " + handDirection);
 
             handDirection.z = 0;
 
-            Debug.Log("Hand position midway = " + handDirection);
+            //Debug.Log("Hand position midway = " + handDirection);
 
             handDirection.Normalize();
 
-            Debug.Log("Hand direction after normalize = " + handDirection);
+            //Debug.Log("Hand direction after normalize = " + handDirection);
 
-            Debug.Log("Hand forward prior = " + transform.forward);
+            //Debug.Log("Hand forward prior = " + transform.forward);
 
-            transform.right = handDirection;
+            float rot = Vector3.Angle(forward, handDirection);
+            Quaternion rotation = Quaternion.Euler(rot, 0, 0);
+            transform.rotation = rotation;
+            Debug.Log("Dir: " + handDirection);
+            Debug.Log("Angle = " + rot);
+            //transform.LookAt(transform.position + handDirection);
+            //transform.right = handDirection;
 
-            Debug.Log("Hand forward after = " + transform.forward);
+            //Debug.Log("Hand forward after = " + transform.forward);
             //angle = Vector3.SignedAngle(transform.position, handDirection, transform.position);
         }
 
         if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
-         {
+        {
             grabbedBy = null;
             grabbed = false;
         }
     }
-    
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "VRHands" && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
