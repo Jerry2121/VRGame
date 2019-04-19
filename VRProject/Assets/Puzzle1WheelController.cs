dadb1184b20ff90c8aabfe39bb40fd5ee95d4869
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-19 08:25:48 -0700

    Updating changes to my scene

--- VRProject/Assets/Puzzle1WheelController.cs
+++ VRProject/Assets/Puzzle1WheelController.cs
@@ -8,6 +8,12 @@ public class Puzzle1WheelController : MonoBehaviour
     public float WheelRotateX;
     public float WheelRotateY;
     // Start is called before the first frame update
+
+    public GameObject grabbedBy;
+    public bool grabbed;
+
+    public float angle;
+
     void Start()
     {
         Wheelposition = this.transform.position;
@@ -16,32 +22,51 @@ public class Puzzle1WheelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (grabbed)
+        {
+            Debug.Log("Hand position prior = " + grabbedBy.transform.position);
+
+            Vector3 handPosition = grabbedBy.transform.position;
+
+            Debug.Log("Hand position after = " + handPosition);
+
+            Vector3 handPositionResetXZ = new Vector3(handPosition.x, handPosition.y, 0);
+
+            Debug.Log("Hand position reset = " + handPositionResetXZ);
+
+            Vector3 handDirection = transform.position - handPositionResetXZ;
+
+            Debug.Log("Hand direction prior = " + handDirection);
+
+            handDirection.z = 0;
+
+            Debug.Log("Hand position midway = " + handDirection);
+
+            handDirection.Normalize();
+
+            Debug.Log("Hand direction after normalize = " + handDirection);
+
+            Debug.Log("Hand forward prior = " + transform.forward);
+
+            transform.forward = handDirection;
+
+            Debug.Log("Hand forward after = " + transform.forward);
+            //angle = Vector3.SignedAngle(transform.position, handDirection, transform.position);
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+         {
+            grabbedBy = null;
+            grabbed = false;
+        }
     }
     
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "VRHands" && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        if (other.gameObject.tag == "VRHands" && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
         {
-            Vector3 handPosition = other.transform.position;
-            Vector3 handPositionResetXZ = new Vector3(handPosition.x, handPosition.y, 0);
-            Vector3 handDirection = transform.position - handPositionResetXZ;
-            handDirection.z = 0;
-            //float angle = Vector3.SignedAngle(handDirection, transform.position)
+            grabbedBy = other.gameObject;
+            grabbed = true;
         }
-    }
-    
-    private void LateUpdate()
-    {
-        this.transform.position = Wheelposition;
-        Quaternion rot = transform.rotation;
-        Vector3 foo = rot.eulerAngles;
-        foo.x = WheelRotateX;
-        foo.y = WheelRotateY;
-        this.transform.rotation = Quaternion.Euler(foo);
-    }
-
-    public void FindHandDirection(float angle)
-    {
-        //transform.rotation = new Vector3(0, -90, angle);
     }
 }
