79407450806f7089d65842fca21d13350db25c22
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-24 09:03:32 -0700

    Puzzle1 Sound Effects

--- VRProject/Assets/Puzzle1Controller.cs
+++ VRProject/Assets/Puzzle1Controller.cs
@@ -10,6 +10,9 @@ public class Puzzle1Controller : MonoBehaviour
     public GameObject PuzzleWheel;
     public Light Puzzle1Light;
     private bool ran1;
+    public AudioSource PuzzlePowerUp;
+    public AudioSource PuzzleWrong;
+    public AudioSource PuzzleCompletedSound;
 
 
     private void Update()
@@ -21,6 +24,7 @@ public class Puzzle1Controller : MonoBehaviour
         if (PuzzlePoweredUp && !ran1)
         {
             Puzzle1Light.color = new Color32(0, 255, 0, 255);
+            PuzzlePowerUp.Play();
             ran1 = true;
         }
     }
@@ -28,17 +32,20 @@ public class Puzzle1Controller : MonoBehaviour
     {
         if (other.gameObject.tag == "P1Start" && !PuzzleStarted && !PuzzleCompleted && PuzzlePoweredUp)
         {
+            Puzzle1Light.color = new Color32(0, 57, 255, 255);
             PuzzleStarted = true;
         }
         if(other.gameObject.tag == "P1Bar" && PuzzleStarted && PuzzlePoweredUp)
         {
             Puzzle1Light.color = new Color32(255, 0, 0, 255);
+            PuzzleWrong.Play();
             PuzzleStarted = false;
 
         }
         if (other.gameObject.tag == "P1End" && PuzzleStarted && PuzzlePoweredUp)
         {
-            Puzzle1Light.color = new Color32(0, 0, 255, 255);
+            Puzzle1Light.color = new Color32(255, 81, 0, 255);
+            PuzzleCompletedSound.Play();
             PuzzleStarted = false;
             PuzzleCompleted = true;
         }
