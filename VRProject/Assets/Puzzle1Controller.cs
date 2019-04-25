60d86a0be2280b9c7b4454f93774d207e2613134
SwordOfWrynn <bubba.wiens@gmail.com>
2019-04-25 09:48:00 -0700

    Work on Puzzel one

--- VRProject/Assets/Puzzle1Controller.cs
+++ VRProject/Assets/Puzzle1Controller.cs
@@ -35,35 +35,60 @@ public class Puzzle1Controller : MonoBehaviour
             ProgressMade = true;
         }
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "P1Start" && !PuzzleStarted && !PuzzleCompleted && PuzzlePoweredUp)
-        {
-            Puzzle1Light.color = new Color32(0, 57, 255, 255);
-            PuzzleStarted = true;
-        }
-        if(other.gameObject.tag == "P1Bar" && PuzzleStarted && PuzzlePoweredUp)
-        {
-            Debug.Log("Puzzle Wrong" + other.gameObject.name);
-            Puzzle1Light.color = new Color32(255, 0, 0, 255);
-            PuzzleWrong.Play();
-            PuzzleStarted = false;
 
-        }
-        if (other.gameObject.tag == "P1End" && PuzzleStarted && PuzzlePoweredUp && ProgressMade)
-        {
-            Puzzle1Light.color = new Color32(0, 65, 9, 255);
-            PuzzleCompletedSound.Play();
-            PuzzleStarted = false;
-            PuzzleCompleted = true;
-        }
-        if (other.gameObject.tag == "P1Progress1")
-        {
-            ProgressMade1 = true;
-        }
-        if (other.gameObject.tag == "P1Progress2")
-        {
-            ProgressMade2 = true;
-        }
+    public void PuzzleStart()
+    {
+        Puzzle1Light.color = new Color32(0, 57, 255, 255);
+        PuzzleStarted = true;
     }
+
+    public void PuzzleFailed()
+    {
+        Puzzle1Light.color = new Color32(255, 0, 0, 255);
+        PuzzleWrong.Play();
+        PuzzleStarted = false;
+        ProgressMade1 = false;
+        ProgressMade2 = false;
+        ProgressMade = false;
+    }
+
+    public void PuzzleComplete()
+    {
+        Puzzle1Light.color = new Color32(0, 65, 9, 255);
+        PuzzleCompletedSound.Play();
+        PuzzleStarted = false;
+        PuzzleCompleted = true;
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "P1Start" && !PuzzleStarted && !PuzzleCompleted && PuzzlePoweredUp)
+    //    {
+    //        Puzzle1Light.color = new Color32(0, 57, 255, 255);
+    //        PuzzleStarted = true;
+    //    }
+    //    if(other.gameObject.tag == "P1Bar" && PuzzleStarted && PuzzlePoweredUp)
+    //    {
+    //        Debug.Log("Puzzle Wrong" + other.gameObject.name);
+    //        Puzzle1Light.color = new Color32(255, 0, 0, 255);
+    //        PuzzleWrong.Play();
+    //        PuzzleStarted = false;
+
+    //    }
+    //    if (other.gameObject.tag == "P1End" && PuzzleStarted && PuzzlePoweredUp && ProgressMade)
+    //    {
+    //        Puzzle1Light.color = new Color32(0, 65, 9, 255);
+    //        PuzzleCompletedSound.Play();
+    //        PuzzleStarted = false;
+    //        PuzzleCompleted = true;
+    //    }
+    //    if (other.gameObject.tag == "P1Progress1")
+    //    {
+    //        ProgressMade1 = true;
+    //    }
+    //    if (other.gameObject.tag == "P1Progress2")
+    //    {
+    //        ProgressMade2 = true;
+    //    }
+    //}
 }
---
+++ VRProject/Assets/_Project/AW/Scripts/Puzzles/TrackElectricPuzzle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackElectricPuzzle : MonoBehaviour
+{
+
+    public Puzzle1Controller puzzleController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
