100ebb90ad8e4826885ef894e7d0ce2eda2174c3
Nathan Weis <DrakonRider598@yahoo.com>
2019-04-25 10:31:18 -0700

    idk

---
+++ VRProject/Assets/_Project/ANW/Scripts/LiquidFill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidFill : MonoBehaviour
+{
+    public float containerFullPos;
+    public float containerEmptyPos;
+
+    public float containerFullScale;
+    public float containerThreeFourths;
+}
