05f778c1d71dd717f96f1fae33f80012020e5100
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-25 10:29:58 -0700

    Fade In and Out Desc

---
+++ VRProject/Assets/RoomFadeText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomFadeText : MonoBehaviour
+{
+    public float TimerFadeInOut;
+    public bool complete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Animator>().SetBool("Fade_In", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!complete)
+        {
+            TimerFadeInOut += Time.deltaTime;
+        }
+        if (TimerFadeInOut >= 3)
+        {
+            complete = true;
+            this.gameObject.GetComponent<Animator>().SetBool("Fade_In", false);
+            this.gameObject.GetComponent<Animator>().SetBool("Fade_Out", true);
+        }
+    }
+}
