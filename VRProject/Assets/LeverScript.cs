3c73e1764ea8cffbd922bbae80674a7c343caa78
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-26 10:01:58 -0700

    Lever Test

---
+++ VRProject/Assets/LeverScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    public float MaxY;
+    public float MinY;
+    Vector3 LevelPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y >= MaxY)
+        {
+            this.transform.position = new Vector3(LevelPos.x, MaxY, LevelPos.z);
+        }
+        if (this.transform.position.y <= MinY)
+        {
+            this.transform.position = new Vector3(LevelPos.x, MinY, LevelPos.z);
+        }
+    }
+}
--- VRProject/Assets/RoomFadeText.cs
+++ VRProject/Assets/RoomFadeText.cs
@@ -5,25 +5,53 @@ using UnityEngine;
 public class RoomFadeText : MonoBehaviour
 {
     public float TimerFadeInOut;
-    public bool complete;
+    public bool RoomNameComplete;
+    public bool RoomObjectiveComplete;
+    public bool ObjectiveComplete;
     // Start is called before the first frame update
     void Start()
     {
         this.gameObject.GetComponent<Animator>().SetBool("Fade_In", true);
+        this.gameObject.GetComponent<Animator>().SetBool("Fade_Out", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!complete)
+        if (!RoomNameComplete)
         {
             TimerFadeInOut += Time.deltaTime;
         }
-        if (TimerFadeInOut >= 3)
+        if (TimerFadeInOut >= 3 && !RoomNameComplete)
         {
-            complete = true;
             this.gameObject.GetComponent<Animator>().SetBool("Fade_In", false);
             this.gameObject.GetComponent<Animator>().SetBool("Fade_Out", true);
+            RoomNameComplete = true;
+            TimerFadeInOut = 0;
+        }
+        if (!RoomObjectiveComplete && ObjectiveComplete)
+        {
+            TimerFadeInOut += Time.deltaTime;
+        }
+        if (ObjectiveComplete && TimerFadeInOut < 3 && !RoomObjectiveComplete)
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("ObjectiveFade_In", true);
+            this.gameObject.GetComponent<Animator>().SetBool("ObjectiveFade_Out", false);
+        }
+        if (TimerFadeInOut >= 3 && ObjectiveComplete && !RoomObjectiveComplete)
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("ObjectiveFade_In", false);
+            this.gameObject.GetComponent<Animator>().SetBool("ObjectiveFade_Out", true);
+            RoomObjectiveComplete = true;
+            TimerFadeInOut = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ObjectiveComplete = true;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            RoomObjectiveComplete = true;
         }
     }
 }
