65e7bc1fff8f4a95ce72a8ce8e6e73c68b01c08a
Jerry2121 <jerry2121121021@yahoo.com>
2019-04-29 10:28:38 -0700

    lever broken

--- VRProject/Assets/LeverScript.cs
+++ VRProject/Assets/LeverScript.cs
@@ -6,6 +6,8 @@ public class LeverScript : MonoBehaviour
 {
     public float MaxY;
     public float MinY;
+    public GameObject grabbedBy;
+    public bool grabbed;
     Vector3 LevelPos;
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,31 @@ public class LeverScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            grabbedBy = null;
+            grabbed = false;
+        }
         if (this.transform.position.y >= MaxY)
         {
-            this.transform.position = new Vector3(LevelPos.x, MaxY, LevelPos.z);
+            
         }
         if (this.transform.position.y <= MinY)
         {
             this.transform.position = new Vector3(LevelPos.x, MinY, LevelPos.z);
+        }
+        if (grabbed)
+        {
+            //LevelPos = this.transform.position;
+            this.transform.position = new Vector3(LevelPos.x, grabbedBy.transform.position.y, LevelPos.z);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "VRHands" && OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            grabbedBy = other.gameObject;
+            grabbed = true;
         }
     }
 }
