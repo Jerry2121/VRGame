04a15df4ed1fb15f71c39c0b4afe848b92f23b8d
Nathan Weis <DrakonRider598@yahoo.com>
2019-05-01 10:11:46 -0700

    Cauldron Puzzle Progress v02

--- VRProject/Assets/_Project/ANW/Scripts/CauldronPuzzle/BottleBehavior.cs
+++ VRProject/Assets/_Project/ANW/Scripts/CauldronPuzzle/BottleBehavior.cs
@@ -4,7 +4,43 @@ using UnityEngine;
 
 public class BottleBehavior : MonoBehaviour
 {
-    public Material[] BottleWrappings;
+    public int liquidID;
+    public Transform bottleTop;
+    public Transform bottleBottom;
 
+    public float addToPourCooldown;
+    float pourCooldown;
 
+    public GameObject cauldronFill;
+
+    private void Start()
+    {
+        pourCooldown = 0f;
+    }
+
+    private void Update()
+    {
+        
+        if (pourCooldown > 0)
+        {
+            pourCooldown -= Time.deltaTime;
+            return;
+        }
+
+        if (bottleBottom.transform.position.y > bottleTop.transform.position.y)
+        {
+            Debug.Log("The bottle is spilling.");
+            RaycastHit hit;
+            if (Physics.Raycast(bottleTop.position, Vector3.down, out hit))
+            {
+                Debug.DrawRay(bottleTop.position, Vector3.down, Color.red);
+                if (hit.collider.tag == "CauldronFill")
+                {
+                    Debug.Log("HIT!");
+                    cauldronFill.GetComponent<CauldronBehavior>().CheckMixture(liquidID);
+                    pourCooldown = 5f;
+                }
+            }
+        }
+    }
 }
--- VRProject/Assets/_Project/ANW/Scripts/CauldronPuzzle/CauldronBehavior.cs
+++ VRProject/Assets/_Project/ANW/Scripts/CauldronPuzzle/CauldronBehavior.cs
@@ -4,13 +4,78 @@ using UnityEngine;
 
 public class CauldronBehavior : MonoBehaviour
 {
-    public Material[] CauldronFills;
-    public bool CorrectMixture;
+    public bool mixtureFinished;
 
-    public 
+    public int correctMixturesNeeded;
+    public int correctMixturesCompleted;
+    public int mixtureNeededID;
+
+    public GameObject cauldronMarking;
+
+    public Color32[] cauldronMarkingsArray;
+    public Color32[] cauldronFillArray;
+
+    float colorLerp;
+    bool lerping;
+
+
+    private void Start()
+    {
+        colorLerp = 0;
+        mixtureNeededID = Random.Range(0, 6);
+        cauldronMarking.GetComponent<Renderer>().material.color = cauldronMarkingsArray[Random.Range(0, 6)];
+    }
 
     void Update()
     {
+        if (correctMixturesCompleted >= correctMixturesNeeded)
+        {
+            mixtureFinished = true;
+            cauldronMarking.GetComponent<Renderer>().material.color = new Color32(0, 0, 0, 0);
+        }
+
+        if (lerping)
+        {
+            colorLerp += Time.deltaTime / 2.0f;
+            cauldronMarking.GetComponent<Renderer>().material.color = Color.Lerp(cauldronMarking.GetComponent<Renderer>().material.color, cauldronMarkingsArray[mixtureNeededID], colorLerp);
+            if (colorLerp >= 1)
+            {
+                lerping = false;
+                colorLerp = 0;
+            }
+        }
+    }
+
+    public void CheckMixture(int liquidID)
+    {
+        if (mixtureFinished)
+        {
+            return;
+        }
+
+        if (liquidID == mixtureNeededID)
+        {
+            correctMixturesCompleted++;
+        }
+        else
+        {
+            correctMixturesCompleted = 0;
+        }
+        UpdateMixture(liquidID);
+    }
+
+    public void UpdateMixture(int liquidID)
+    {
+        mixtureNeededID = Random.Range(0, 6);
         
+        if (mixtureNeededID == liquidID)
+        {
+            UpdateMixture(liquidID);
+            return;
+        }
+
+        lerping = true;
     }
 }
+
+
