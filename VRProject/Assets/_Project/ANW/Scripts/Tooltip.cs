079ec3f889ce1ebaf4ee3f1f6c071202ccbd1de9
Jerry2121 <jerry2121121021@yahoo.com>
2019-05-06 09:58:33 -0700

    ToolTip Test #1

--- VRProject/Assets/_Project/ANW/Scripts/Tooltip.cs
+++ VRProject/Assets/_Project/ANW/Scripts/Tooltip.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Tooltip : MonoBehaviour
 {
+    public GameObject ToolTipCanvas;
     // public TextMesh tooltip;
     // public TextMesh hint;
 
     // public void EnableUI() {
     // tooltip.SetActive = true;
     // if (hints are enabled) { hint.SetActive = true; } }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ToolTipCanvas.SetActive(!ToolTipCanvas.activeSelf);
+        }
+    }
 }
