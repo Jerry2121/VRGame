8a33aad114e364249387cb4547ad855909fbff07
Nathan Weis <DrakonRider598@yahoo.com>
2019-05-07 10:31:40 -0700

    Scale puzzle progress v01

---
+++ VRProject/Assets/_Project/ANW/Scripts/ObjectBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehavior : MonoBehaviour
+{
+    public float objectWeight;
+    public float delayEffectDuration;
+    public bool weightApplied;
+}
---
+++ VRProject/Assets/_Project/ANW/Scripts/ScaleBehavior.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBehavior : MonoBehaviour
+{
+    public Vector3 originalPosition;
+    public float weightHeld;
+    public GameObject partnerScale;
+    public float pushDown;
+    public float maxPushDown;
+    public float levelComparisonBalance;
+    [HideInInspector] public bool pressurePlatePressed;
+    public bool adminScale;
+    bool puzzleComplete;
+    public GameObject prize;
+    public Transform prizeSpawnLocation;
+    public bool prizeSpawned;
+
+    private void Start()
+    {
+        originalPosition = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        if (prizeSpawned)
+        {
+            return;
+        }
+
+        if (puzzleComplete && adminScale)
+        {
+            Instantiate(prize, prizeSpawnLocation);
+            prizeSpawned = true;
+            partnerScale.GetComponent<ScaleBehavior>().prizeSpawned = true;
+        }
+
+        pushDown = Mathf.Lerp(0.0f, (weightHeld - partnerScale.GetComponent<ScaleBehavior>().weightHeld / levelComparisonBalance), 0.005f);
+        if(pushDown == 0)
+        {
+            pushDown = -Mathf.Lerp(0.0f, originalPosition.y - transform.position.y, 0.005f);
+        }
+        Debug.Log("position - maxPushDown = " + (originalPosition.y - maxPushDown));
+        gameObject.transform.position = new Vector3(transform.position.x, transform.position.y - pushDown, transform.position.z);
+
+        if (gameObject.transform.position.y <= originalPosition.y - maxPushDown)
+        {
+            gameObject.transform.position = new Vector3(originalPosition.x, originalPosition.y - maxPushDown, originalPosition.z);
+        }
+
+        else if (gameObject.transform.position.y >= originalPosition.y + maxPushDown)
+        {
+            gameObject.transform.position = new Vector3(originalPosition.x, originalPosition.y + maxPushDown, originalPosition.z);
+        }
+        
+
+        if (!pressurePlatePressed && partnerScale.GetComponent<ScaleBehavior>().pressurePlatePressed)
+        {
+            puzzleComplete = true;
+        }
+
+
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "PressurePlate")
+        {
+            pressurePlatePressed = true;
+        }
+
+        if (other.gameObject.GetComponent<ObjectBehavior>() == true && !other.gameObject.GetComponent<ObjectBehavior>().weightApplied && !other.gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+                weightHeld += other.gameObject.GetComponent<ObjectBehavior>().objectWeight;
+                other.gameObject.GetComponent<ObjectBehavior>().weightApplied = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PressurePlate")
+        {
+            pressurePlatePressed = false;
+        }
+
+        if (other.gameObject.GetComponent<ObjectBehavior>() == true && other.gameObject.GetComponent<ObjectBehavior>().weightApplied)
+        {
+            weightHeld -= other.gameObject.GetComponent<ObjectBehavior>().objectWeight;
+            other.gameObject.GetComponent<ObjectBehavior>().weightApplied = false;
+        }
+    }
+}
