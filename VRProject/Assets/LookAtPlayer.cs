38e591c6d3bd45dab39174c01887fef0826bd21e
Jerry2121 <jerry2121121021@yahoo.com>
2019-05-08 08:31:48 -0700

    Interaction indicator image changed and made it look at the player

---
+++ VRProject/Assets/LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("LocalPlayer").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player);
+    }
+}
