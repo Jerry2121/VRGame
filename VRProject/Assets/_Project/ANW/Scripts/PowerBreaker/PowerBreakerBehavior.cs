1e62ab419c8ad1d9537f458ff8c600328cb77bfa
Nathan Weis <DrakonRider598@yahoo.com>
2019-05-10 10:32:05 -0700

    I don't know what I changed

---
+++ VRProject/Assets/_Project/ANW/Scripts/PowerBreaker/PowerBreakerBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBreakerBehavior : MonoBehaviour
+{
+    public int[] buttonIDsNumeric;
+    public int[] buttonIDsColored;
+
+    public int correctMatchesCompleted;
+
+    public int numericButtonPressedID;
+    public int coloredButtonPressedID;
+
+    public void CheckButtonMatch()
+    {
+        if (numericButtonPressedID == coloredButtonPressedID)
+        {
+            correctMatchesCompleted++;
+        }
+
+        else if (numericButtonPressedID != coloredButtonPressedID)
+        {
+            correctMatchesCompleted = 0;
+        }
+    }
+}
