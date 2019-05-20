12b2c0c012af2a86026d733a6fbc63c74fae4cb0
Nathan Weis <DrakonRider598@yahoo.com>
2019-05-20 10:34:46 -0700

    Room2 Progress

---
+++ VRProject/Assets/_Project/ANW/Scripts/BookCover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookCover : MonoBehaviour
+{
+    public Material[] bookCovers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material[] tempMaterials = gameObject.GetComponent<MeshRenderer>().materials;
+        tempMaterials[1] = bookCovers[Random.Range(1, bookCovers.Length)];
+        gameObject.GetComponent<MeshRenderer>().materials = tempMaterials;
+    }
+}
