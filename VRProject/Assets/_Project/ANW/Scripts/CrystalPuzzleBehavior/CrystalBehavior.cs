201b21f10176c2062684d5796c6ac9393e821c33
Nathan Weis <DrakonRider598@yahoo.com>
2019-05-22 08:30:04 -0700

    Room3Objects and CrystalPuzzle progress

---
+++ VRProject/Assets/_Project/ANW/Scripts/CrystalPuzzleBehavior/CrystalBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalBehavior : MonoBehaviour
+{
+    float recentlyGrabbedTimer;
+    bool recentlyGrabbed;
+
+    private void Update()
+    {
+        if (gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            recentlyGrabbedTimer = 1.0f;
+        }
+
+        else if (!gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            if (recentlyGrabbedTimer > 0)
+            {
+                recentlyGrabbed = true;
+                recentlyGrabbedTimer -= Time.deltaTime;
+            }
+
+            else if (recentlyGrabbedTimer < 0)
+            {
+                recentlyGrabbed = false;
+                recentlyGrabbedTimer = 0;
+            }
+        }
+    }
+
+   void OnTriggerStay(Collider other)
+    {
+        {
+            if (other.gameObject.GetComponent<CrystalPuzzleBehavior>() && recentlyGrabbed)
+            {
+                GameObject g = Instantiate(gameObject, other.gameObject.transform);
+                other.gameObject.GetComponent<CrystalPuzzleBehavior>().crystalRecieved = g;
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
