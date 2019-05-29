0e87db37a6d0aca1316848fc4a1803470990796a
Jerry2121 <jerry2121121021@yahoo.com>
2019-05-29 09:49:07 -0700

    Pushing

---
+++ VRProject/Assets/TimerScr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerScr : MonoBehaviour
+{
+    public float Timer = 10f;
+    public TextMeshProUGUI GameClosingText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameClosingText = this.gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer > 0)
+        {
+            Timer -= Time.deltaTime;
+        }
+
+        double b;
+        b = System.Math.Round(Timer, 0);
+        GameClosingText.text = "Gaming Closing in " + b + " Seconds...";
+        if (Timer <= 0)
+        {
+            Application.Quit();
+        }
+    }
+}
