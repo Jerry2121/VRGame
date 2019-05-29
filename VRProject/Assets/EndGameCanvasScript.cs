46623d19208cd510541b21ff45c1b65b80df8c02
Jerry2121 <jerry2121121021@yahoo.com>
2019-05-29 07:52:50 -0700

    Blah

---
+++ VRProject/Assets/EndGameCanvasScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameCanvasScript : MonoBehaviour
+{
+        public GameObject EndGameCanvas;
+    public GameObject WinObject;
+    public GameObject LooseObject;
+    public Camera CenterEyeCamera;
+    public GameObject GameOverTitle;
+    public GameObject GameOverClosing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- VRProject/Assets/LeverScript.cs
+++ VRProject/Assets/LeverScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRGame.Networking;
 
 public class LeverScript : MonoBehaviour
 {
@@ -12,6 +13,14 @@ public class LeverScript : MonoBehaviour
     Vector3 LevelRot;
     Quaternion LeverRot;
     public bool leverActivated;
+    public GameObject OtherLever;
+    public GameObject EndGameCanvas;
+    public GameObject WinObject;
+    public GameObject LooseObject;
+    public Camera CenterEyeCamera;
+    public GameObject GameOverTitle;
+    public GameObject GameOverClosing;
+    private static bool ran;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +32,23 @@ public class LeverScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (EndGameCanvas == null)
+        {
+            if (NetworkingManager.GetLocalPlayer() != null)
+            {
+                EndGameCanvasScript canvasscript = NetworkingManager.GetLocalPlayer().GetComponentInChildren<EndGameCanvasScript>();
+                EndGameCanvas = canvasscript.gameObject;
+                WinObject = canvasscript.WinObject;
+                LooseObject = canvasscript.LooseObject;
+                CenterEyeCamera = canvasscript.CenterEyeCamera;
+                GameOverTitle = canvasscript.GameOverTitle;
+                GameOverClosing = canvasscript.GameOverClosing;
+            }
+            else
+            {
+                return;
+            }
+        }
         transform.rotation = LeverRot;
         if (OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger) || OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
         {
@@ -43,6 +69,44 @@ public class LeverScript : MonoBehaviour
                 leverActivated = true;
             }
             
+        }
+        //If Player 1 Pulls Lever and Wins
+        if (leverActivated && this.gameObject.name == "1LeverHandle" && !OtherLever.GetComponent<LeverScript>().leverActivated && !ran)
+        {
+            GameOverClosing.SetActive(true);
+            GameOverTitle.SetActive(true);
+            WinObject.SetActive(true);
+            LooseObject.SetActive(false);
+            CenterEyeCamera.cullingMask = 1 << 12;
+            ran = true;
+        }
+        //If Player 2 Pulls Lever and Wins
+        if (leverActivated && this.gameObject.name == "2LeverHandle" && !OtherLever.GetComponent<LeverScript>().leverActivated && !ran)
+        {
+            GameOverClosing.SetActive(true);
+            GameOverTitle.SetActive(true);
+            WinObject.SetActive(true);
+            LooseObject.SetActive(false);
+            CenterEyeCamera.cullingMask = 1 << 12;
+            ran = true;
+        }
+        //If Player 2 Pulls Lever, Player 1 Looses
+        if (!leverActivated && this.gameObject.name == "1LeverHandle" && OtherLever.GetComponent<LeverScript>().leverActivated && ran)
+        {
+            GameOverClosing.SetActive(true);
+            GameOverTitle.SetActive(true);
+            WinObject.SetActive(false);
+            LooseObject.SetActive(true);
+            CenterEyeCamera.cullingMask = 1 << 12;
+        }
+        //If Player 1 Pulls Lever, Player 2 Looses
+        if (!leverActivated && this.gameObject.name == "2LeverHandle" && OtherLever.GetComponent<LeverScript>().leverActivated && ran)
+        {
+            GameOverClosing.SetActive(true);
+            GameOverTitle.SetActive(true);
+            WinObject.SetActive(false);
+            LooseObject.SetActive(true);
+            CenterEyeCamera.cullingMask = 1 << 12;
         }
     }
     private void OnTriggerStay(Collider other)
